1198e4ae166bed0ef7975ddf27224602a0caf744
WTL123456 <18239558109@163.com>
2018-10-30 16:22:36 +0800

    测试

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test01
+{
+    public class Student
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
